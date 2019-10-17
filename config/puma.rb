72176aaf4ceb8853b0a74363cefa6af3868f79a0
“T-Aruga” <baideneo@gmail.com>
2019-10-17 16:55:02 +0000

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -40,6 +40,23 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
 # end
 
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
 # process is booted, this block will be run. If you are using the `preload_app!`
