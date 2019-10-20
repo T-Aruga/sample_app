33a86d6d971d0e4a73fd2f0992a91dfc4aa34a96
“T-Aruga” <baideneo@gmail.com>
2019-10-20 07:30:31 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+before_action :correct_user,   only: [:edit, :update]
+before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -19,11 +27,49 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+   def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+   end
+   
+   def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
+   end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user                                                            # 正しいユーザーかどうか確認
+      @user = User.find(params[:id])                                            # URLのidの値と同じユーザーを@userに代入
+      redirect_to(root_url) unless current_user?(@user)                         # @userと記憶トークンcookieに対応するユーザー(current_user)を比較して、失敗したらroot_urlへリダイレクト
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
